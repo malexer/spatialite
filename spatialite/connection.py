768f44a88f55635b6495a33b62ac434ee91e3314
Oleksii Markov <alex@markovs.me>
2018-04-04 15:43:48 +0300

    Added SpatialMetadata class

--- spatialite/connection.py
+++ spatialite/connection.py
@@ -1,8 +1,69 @@
+from itertools import chain
 import sqlite3
 
 
 class LoadExtensionError(sqlite3.Error):
     pass
+
+
+class SpatialMetadata(object):
+
+    TABLES = (
+        'ElementaryGeometries',
+        'SpatialIndex',
+        'geometry_columns',
+        'geometry_columns_auth',
+        'geometry_columns_field_infos',
+        'geometry_columns_statistics',
+        'geometry_columns_time',
+        'spatial_ref_sys',
+        'spatial_ref_sys_aux',
+        'spatialite_history',
+        'sql_statements_log',
+        'views_geometry_columns',
+        'views_geometry_columns_auth',
+        'views_geometry_columns_field_infos',
+        'views_geometry_columns_statistics',
+        'virts_geometry_columns',
+        'virts_geometry_columns_auth',
+        'virts_geometry_columns_field_infos',
+        'virts_geometry_columns_statistics',
+    )
+
+    VIEWS = (
+        'geom_cols_ref_sys',
+        'spatial_ref_sys_all',
+        'vector_layers',
+        'vector_layers_auth',
+        'vector_layers_field_infos',
+        'vector_layers_statistics',
+    )
+
+    def __init__(self, connection):
+        self.db = connection
+
+    def initialize(self):
+        """Create and populate SpatiaLite metadata tables."""
+        self.db.transaction('SELECT InitSpatialMetadata()', level='EXCLUSIVE')
+
+    def drop(self, auto_vacuum=True):
+        self.db.transaction(
+            '; '.join(chain(
+                ('DROP VIEW IF EXISTS %s' % t for t in self.VIEWS),
+                ('DROP TABLE IF EXISTS %s' % t for t in self.TABLES),
+            )),
+            level='EXCLUSIVE',
+        )
+        if auto_vacuum:
+            self.db.execute('VACUUM;')
+
+    def recover(self, table, column):
+        query = (
+            "SELECT InitSpatialMetadata();"
+            "SELECT RecoverGeometryColumn('%s', '%s', 4326, 'POINT');"
+        )
+        query %= (table, column)
+        self.db.transaction(query, level='EXCLUSIVE')
 
 
 class Connection(sqlite3.Connection):
@@ -15,6 +76,7 @@ class Connection(sqlite3.Connection):
     def __init__(self, *args, **kwargs):
         super(Connection, self).__init__(*args, **kwargs)
         self.enable_spatialite_extension()
+        self._metadata = SpatialMetadata(self)
 
     def enable_spatialite_extension(self):
         # requires mod_spatialite
@@ -49,7 +111,7 @@ class Connection(sqlite3.Connection):
 
     def initialize_metadata(self):
         """Create and populate SpatiaLite metadata tables."""
-        self.transaction('SELECT InitSpatialMetadata()', level='IMMEDIATE')
+        self._metadata.initialize()
 
 
 def connect(*args, **kwargs):
